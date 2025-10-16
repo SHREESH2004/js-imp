770573e6d3064bb57ec03a64e3f75f3d9cf36de3
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-16 20:13:04 +0530

    chnages

---
+++ obj.js
@@ -0,0 +1,9 @@
+let obj={
+    name:"abc",
+    age:20,
+}
+obj.name;//access
+obj["name"];//access
+obj["name"]="xyz"//update
+obj.city="delhi"//add new key value pair
+delete obj.age;//delete key value pair
