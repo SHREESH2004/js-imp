cdef5eb383d28a356c2021aadd2208013e9696ae
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-16 20:29:16 +0530

    s

--- obj.js
+++ obj.js
@@ -1,6 +1,15 @@
 let obj={
     name:"abc",
     age:20,
+    address:{
+        city:"delhi",
+        state:"dl",
+        pincode:110001,
+        location:{
+            lat:123,
+            long:456
+    }
+}
 }
 obj.name;//access
 obj["name"];//access
