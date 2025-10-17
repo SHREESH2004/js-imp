eb7bb18dfa63833941fe3be2e0606d92e7aab070
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-18 01:44:29 +0530

    object destructuring added

--- obj.js
+++ obj.js
@@ -19,3 +19,16 @@ console.log(obj);
 
 //destrucutring
 let {lat,long}=obj.address.location;
+
+for (let ash in obj){
+    console.log(ash,obj[ash]);
+}
+
+//if obj.key then searching for the valu of key which named as key and if want to search if in loop use obj[key] where key is variable storing the key name
+let keys=Object.keys(obj);
+console.log(keys);
+
+//Object.entries to give key value par all entri lik[name,"abc"] top it wil show Array[1],Array[2]... like this
+let entries=Object.entries(obj);
+console.log(entries);
+
