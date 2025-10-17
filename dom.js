dbe81cc11740162e47db472158ff7968ffc4c0f5
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-18 02:21:08 +0530

    added documentations

---
+++ dom.js
@@ -0,0 +1,6 @@
+//Documentation for DOM manipulation functions
+//every thing in html is a node
+//node types: element node, text node, comment node, document node
+//Element node: represents an element in the HTML document
+// All types are part of the DOM (Document Object Model)
+
