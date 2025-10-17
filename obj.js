ae8cc49a452490f62868b9027b02e78ef990f322
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-18 02:05:34 +0530

    add ons

--- obj.js
+++ obj.js
@@ -32,3 +32,30 @@ console.log(keys);
 let entries=Object.entries(obj);
 console.log(entries);
 
+let obj2={...obj} //spread operator to copy object
+console.log(obj2); 
+
+let obj3=Object.assign({price:Infinity},obj); //another way to copy object
+console.log(obj3);
+//same as obj2 using spread operator not use too much now basically it add the bj to theobj present in assign {}
+
+console.log(JSON.stringify(obj3)); //convert object to string
+console.log(JSON.parse(JSON.stringify(obj3))); //string to object
+
+console.log(obj.address?.location?.lat); //optional chaining if any of the property is undefined it will return undefined instead of error
+//used when we r not sure if the property is present or not
+
+
+//Question: Can a object key be an object? No, it will be converted to string [object Object]
+let obj4={};
+let keyObj={id:1};
+obj4[keyObj]="value";
+console.log(obj4); //{ '[object Object]': 'value' }
+//Can an object key be boolean :yes it will be converted to string
+//can an be map key be object : yes
+let map=new Map();
+let keyObj2={id:2};
+map.set(keyObj2,"mapValue");
+console.log(map.get(keyObj2)); //mapValue
+
+//JSON.Stringify then JSON.parse for deep copy and spread operator gives reference so shallow copy
