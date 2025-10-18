630a37e6d89d4b1a3ffa10be1c01e6af1a20ff30
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-18 16:25:29 +0530

    add ons for dom

--- dom.js
+++ dom.js
@@ -4,3 +4,11 @@
 //Element node: represents an element in the HTML document
 // All types are part of the DOM (Document Object Model)
 
+const title=document.getElementById('main-title');
+console.dir(title);
+console.log(title.id);
+console.log(title.innerHTML);
+const queryTitle=document.querySelector('#main-title');// select fist elemt with the id
+console.log(queryTitle.innerHTML);
+const queryall=document.querySelectorAll('#main-title');// select all elements with the id
+console.log(queryall);//give all elements with the id in a node list in an array-like structure
