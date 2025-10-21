25e2a92a8c908c238b5c2be186138c0f3a04b532
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-21 08:28:50 +0530

    scopes

---
+++ backend_basics/scope.js
@@ -0,0 +1,13 @@
+//scorpe: created variables and fcntion can be used in what classes or functions
+
+function abcd(){
+    var a = 10; // function scope
+}
+console.log(a); // error: a is not defined
+
+//scope 3 types: function scope, block scope, global scope
+//function scope: variables declared inside a function are not accessible outside the function
+//block scope: variables declared inside a block (e.g., inside curly braces {}) are not accessible outside the block
+//global scope: variables declared outside any function or block are accessible everywhere in the code
+
+
