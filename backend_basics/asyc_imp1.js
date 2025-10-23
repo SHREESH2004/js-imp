ecb40a5ce1fa4d71c5e1ae2bfc23716c174c6063
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-24 02:27:24 +0530

    callback hell

--- backend_basics/asyc_imp1.js
+++ backend_basics/asyc_imp1.js
@@ -1,13 +1,16 @@
 function createinvoice(callback) {
     setTimeout(() => {
         console.log("Creating invoice");
-        callback();
+        const error=null;
+        const chargeamount=500;
+        callback(error, chargeamount);
     }, 2000);
 }
 function servingfood(callback) {
     setTimeout(() => {
         console.log("Serving food")
-        callback();
+        const error=new Error("Food is not served");
+        callback(error);
     }, 1000)
 }
 function checkinventory(callback) {
@@ -20,11 +23,17 @@ function main() {
     console.log("Other process running");
     checkinventory(() => {
         console.log("Inventory done")
-        servingfood(() => {
-            createinvoice(() => {
-                servingfood(() => {
-                    console.log("Food is served");
-                });
+        servingfood((error) => {
+            if (error) {
+                console.error(error.message);
+            }
+            createinvoice((err, chargeamount) => {
+                if (err) {
+                    console.error(err.message);
+                    return;
+                }
+                console.log("Invoice created");
+                console.log(`Charge amount: ${chargeamount}`);
             });
         });
     });
