ae84bf38ee99110ae44aa818aec7678c895651ad
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-23 23:39:22 +0530

    263

--- backend_basics/asyc_imp1.js
+++ backend_basics/asyc_imp1.js
@@ -1,14 +1,33 @@
-function createinvoice(){
-    console.log("Creating invoice");
+function createinvoice(callback) {
+    setTimeout(() => {
+        console.log("Creating invoice");
+        callback();
+    }, 2000);
 }
-function servingfood(){
-    console.log("Serving food")
+function servingfood(callback) {
+    setTimeout(() => {
+        console.log("Serving food")
+        callback();
+    }, 1000)
+}
+function checkinventory(callback) {
+    setTimeout(() => {
+        console.log("Serving food");
+        callback();
+    })
+}
+function main() {
+    console.log("Other process running");
+    checkinventory(() => {
+        console.log("Inventory done")
+        servingfood(() => {
+            createinvoice(() => {
+                servingfood(() => {
+                    console.log("Food is served");
+                });
+            });
+        });
+    });
 }
 
-function main(){
-    createinvoice();
-    servingfood();
-}
-//example of synchronous function
-
-main()
+main();
