13d284e332f775611002264e9603928832088f5c
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-23 16:39:33 +0530

    some changes

---
+++ backend_basics/asyc_imp1.js
@@ -0,0 +1,13 @@
+function createinvoice(){
+    console.log("Creating invoice");
+}
+function servingfood(){
+    console.log("Serving food")
+}
+
+function main(){
+    createinvoice();
+    servingfood();
+}
+//example of synchronous function
+
