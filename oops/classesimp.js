60289660a57fc37e6b1b90e409c0629c85a02689
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-25 10:08:23 +0530

    oops concepts

---
+++ oops/classesimp.js
@@ -0,0 +1,26 @@
+class Car {
+    constructor(brand, model, year, role) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.role = role;
+    }
+}
+
+class Admin {
+    // 1. The constructor takes one argument: a 'Car' object
+    constructor(carObject) {
+        this.car = carObject; 
+        this.role = "admin";
+    }
+
+    Alldetails() {
+        console.log("--- Admin is accessing the Car's details ---");
+        console.log([this.car.brand,this.car.model,this.car.year,this.role]);
+    }
+}
+
+const myCar = new Car("BMW", "Gt20", 2025,"user");
+const Admins = new Admin(myCar);
+
+Admins.Alldetails();
